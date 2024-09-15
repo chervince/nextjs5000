9951df00ddef74f4832eb9d3ed58832ded196aeb
Chervince <33560587+chervince@users.noreply.github.com>
2024-09-15 15:55:24 +1100

    home page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-// app/page.tsx
-
 import React from 'react';
 import Link from 'next/link';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Accueil</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div>
+        <h1>Accueil</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <img src="https://lmvride.nc/wp-content/uploads/2024/04/visuel_web_ride_camp_LMV-finalPlan-de-travail-1-1024x1024.png" alt="Visuel Ride Camp" />
+      </div>
+    </>
   );
 };
 
