bf43985fbad146973a15161dd5712707a554bdc1
Chervince <33560587+chervince@users.noreply.github.com>
2024-09-16 23:10:52 +1100

    bg-black : dashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black-100">
             <h1 className="text-2xl font-bold mb-4">
                 Bienvenue, {user.email ?? 'Utilisateur inconnu'} !
             </h1>
